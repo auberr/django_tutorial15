506611178c1ef1ebd460f18fd37908a9720011d3
auberr <auberr@daum.net>
2022-11-15 09:24:29 +0900

    test done

--- ai2_back/settings.py
+++ ai2_back/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os, json
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%($@&p^lr6kvh6--azh4$nmj3^r(ocn8!hz*)9mj0ef!!1=&r2'
+
+secret_file = os.path.join(BASE_DIR, 'secrets.json') 
+
+with open(secret_file) as f:
+    secrets = json.loads(f.read())
+
+def get_secret(setting, secrets=secrets):
+    try:
+        return secrets[setting]
+    except KeyError:
+        error_msg = "Set the {} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
+
+SECRET_KEY = get_secret("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -151,10 +167,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = '/media/' 
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
-
+from django.urls import reverse
+from rest_framework.test import APITestCase
 # Create your tests here.
+
+class UserRegistrationAPIViewTestCase(APITestCase):
+    def test_registration(self):
+        url = reverse("user_view")
+        user_data = {
+            "username":"testuser1",
+            "password":"password",
+            "email":"test@user.com",
+            "fullname":"kim"
+        }
+        response = self.client.post(url, user_data)
+        self.assertEqual(response.data['message'], "가입 완료!!")
